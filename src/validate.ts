521c41eda5649701afb6fa1c00c18521eb5ba790
Laurent Stéphenne <laurent@guibi.dev>
2023-06-26 23:18:32 -0400

    Added all files

---
+++ src/validate.ts
@@ -0,0 +1,20 @@
+import type { RequestEvent } from "@sveltejs/kit";
+import type { z } from "zod";
+import { createApiFetch } from "./fetch";
+
+export async function apiValidate(data: object, schema: z.AnyZodObject, f?: typeof fetch) {
+    const json = await parseRequestData(data);
+    const parse = schema.safeParse(json);
+
+    return { parse, apiFetch: createApiFetch(f ?? fetch) };
+}
+
+async function parseRequestData(data: RequestEvent | Request | object) {
+    if (data instanceof Request) {
+        return data.json();
+    } else if ("request" in data && data.request instanceof Request) {
+        return data.request.json();
+    } else {
+        return data;
+    }
+}
